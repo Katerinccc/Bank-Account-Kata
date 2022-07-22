6a5a1291954f66e52fa3632c2e4cb1bde653263d
Katerin Calderon <kccc0713@gmail.com>
2022-07-21 20:16:24 -0500

    initial project setup

---
+++ src/com/sofka/StartApp.java
@@ -0,0 +1,11 @@
+package com.sofka;
+
+import com.sofka.entities.Account;
+
+public class StartApp {
+    public static void main(String[] args) {
+
+        Account account = new Account();
+
+    }
+}
---
+++ src/com/sofka/entities/Account.java
@@ -0,0 +1,25 @@
+package com.sofka.entities;
+
+public class Account {
+
+    private Statement statement;
+    private int balance;
+
+
+    public Account() {
+        this.statement = new Statement();
+    }
+
+    public void deposit() {
+
+    }
+
+    public void withdraw(){
+
+    }
+
+    public void printStatements() {
+
+    }
+
+}
